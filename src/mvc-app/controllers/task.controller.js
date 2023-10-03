153e3806203afdb3d887568789f0bc8d4b1b4376
tantran <duytantran.it@gmail.com>
2023-10-03 13:05:29 +0700

    Update logic for model, optimize the controller

--- src/mvc-app/controllers/task.controller.js
+++ src/mvc-app/controllers/task.controller.js
@@ -4,75 +4,45 @@ class TaskController {
   constructor(model) {
     this.model = model;
     this.LocalStorageService = new LocalStorageService();
-    this.newData = this.LocalStorageService.getFromLocal();
   }
 
   readData() {
-    const taskDatas = this.model.taskDatas;
-    this.LocalStorageService.saveToLocal(taskDatas);
-    this.model.restructureData(this.newData);
-    return this.model.taskDatas;
+    const newDatas = this.LocalStorageService.getFromLocal();
+    if (newDatas == null) {
+      this.LocalStorageService.saveToLocal(this.model.taskDatas);
+      console.log(this.model.taskDatas);
+    } else {
+      this.LocalStorageService.saveToLocal(newDatas);
+      this.model.restructureData(newDatas);
+      return this.model.taskDatas;
+    }
   };
 
   addNewData(title, status) {
-    const taskDatas = this.model.taskDatas;
-    for (const key in taskDatas) {
-      if (key == status) {
-        const uniqueId = Math.floor(Math.random() * 3000);
-        taskDatas[key].push({
-          id: uniqueId,
-          title: title,
-          status: status
-        });
-      }
-    }
-
+    const uniqueId = new Date().getTime();
+    const taskDatas = this.model.addNew(
+      { id: uniqueId, title, status },
+      status
+    );
     this.LocalStorageService.saveToLocal(taskDatas);
-    this.model.restructureData(this.newData);
+    const newDatas = this.LocalStorageService.getFromLocal();
+    this.model.restructureData(newDatas);
   };
 
-  deleteData(index, status) {
-    const taskDatas = this.model.taskDatas;
-    for (const key in taskDatas) {
-      if (status == key) {
-        taskDatas[key].splice(index, 1);
-      }
-    }
-
+  deleteData(id, status) {
+    const taskDatas = this.model.delete(id, status);
     this.LocalStorageService.saveToLocal(taskDatas);
-    this.model.restructureData(this.newData);
+    const newDatas = this.LocalStorageService.getFromLocal();
+    this.model.restructureData(newDatas);
   };
 
   updateData(id, status, inputTitle, inputStatus) {
-    const taskDatas = this.model.taskDatas;
-    const task = this.model.getData(id, status);
-    // Demo change title effect
-    task.title = inputTitle;
-    task.status = inputStatus;
-
-    for (const key in taskDatas) {
-      if (key == status) {
-        const statusArray = taskDatas[key];
-        for (let index = 0; index < statusArray.length; index++) {
-          const object = statusArray[index];
-          if (object.id == id) {
-            statusArray.splice(index, 1);
-            for (const key in taskDatas) {
-              if (key == object.status) {
-                const statusArray = taskDatas[key];
-                statusArray.push({
-                  id: object.id,
-                  title: object.title,
-                  status: object.status
-                });
-              }
-            }
-          }
-        }
-      }
-    }
+    const taskDatas = this.model.update(
+      id, status, inputTitle, inputStatus
+    );
     this.LocalStorageService.saveToLocal(taskDatas);
-    this.model.restructureData(this.newData);
+    const newDatas = this.LocalStorageService.getFromLocal();
+    this.model.restructureData(newDatas);
   };
 
 }
