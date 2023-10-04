a742149ddda28099f50ea0e627accda5de9b8445
tantran <duytantran.it@gmail.com>
2023-10-04 09:05:06 +0700

    Optimize and update comments for model and controller

--- src/mvc-app/models/task.model.js
+++ src/mvc-app/models/task.model.js
@@ -11,11 +11,14 @@ class TaskModel extends Observable {
     };
   }
 
+  // Load data from Local Storage -> to Service -> finally this.taskDatas
   restructureData(data) {
     this.taskDatas = data;
     this.notify(this.taskDatas);
   }
 
+  // Define exactly object in status array -> assign important information for modal tag's attribute
+  // *Copy the value of the object to manipulate it through the Modal*
   getData(id, status) {
     if (id && status) {
       const target = this.taskDatas[status];
@@ -36,22 +39,22 @@ class TaskModel extends Observable {
   }
 
   update(id, status, newTitle, newStatus, newUpdateDate) {
-    // Find object with id
+    // Find exactly object with id
     const currentArray = this.taskDatas[status];
     const currentTask = currentArray.find((task) => task.id == id);
 
-    // Condition display error
+    // Condition to display error in console
     if (!currentTask) throw new Error('Task is not exist!');
 
-    // If status === new Status -> Just update content with new Title
+    // If Task's status === new Status -> Just update content with new Title
     // Then sort Array by update Date
     if (status === newStatus) {
       currentTask.title = newTitle;
       currentTask.updateDate = newUpdateDate;
-      // Sort array by updateDate
+      // Sort array from past to present based on updateDate
       currentArray.sort((objA, objB) => new Date(objA.updateDate) - new Date(objB.updateDate));
     } else {
-      // If status != new Status -> remove current Task -> add to new Array and update new status && new title
+      // If Task's status != new Status -> remove current Task -> add to new Array and update new status && new title
       const newCurrentArray = currentArray.filter((task) => task.id !== id);
       currentTask.title = newTitle;
       currentTask.status = newStatus;
@@ -59,6 +62,7 @@ class TaskModel extends Observable {
       const target = this.taskDatas[newStatus];
       target.push(currentTask);
 
+      // Re-update data
       this.taskDatas = {
         ...this.taskDatas,
         [`${status}`]: newCurrentArray,
@@ -66,12 +70,12 @@ class TaskModel extends Observable {
       };
     }
     this.notify(this.taskDatas);
-
     return this.taskDatas;
   }
 
   delete(id, status) {
     const currentArray = this.taskDatas[status];
+    // Filter to remove Task and re-update data
     const newCurrentArray = currentArray.filter((task) => task.id != id);
     this.taskDatas = {
       ...this.taskDatas,
